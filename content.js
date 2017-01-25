9de92238daf8fd33ce1c5705ab15d3e22388caa2
Alex Efremov <alex.efremov.dev@gmail.com>
2017-01-25 18:03:32 +0200

    Change title on click

--- content.js
+++ content.js
@@ -1,5 +1,7 @@
 var controlBox = document.getElementsByClassName("bottom-left")[0];
 var targetBox = document.getElementsByClassName("bottom-right")[0];
+var title = document.getElementsByClassName("title")[0];
 controlBox.addEventListener("click", function() {
   targetBox.remove();
+  title.innerHTML = "OMG?! Did you see it, guys?! It was magic!"
 });
